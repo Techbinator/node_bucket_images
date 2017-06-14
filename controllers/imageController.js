286c9cc243195d272686f0c791b486637d837ec8
tfilipovici <tudor.filipovici@holidaycheck.com>
2017-06-14 18:18:23 +0200

    Remove some extra test data

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,12 +1,12 @@
-var imageHelper = require('../helpers/image'),
-    testImage = 'http://orig01.deviantart.net/92d3/f/2010/110/7/2/pla_sf_3_by_recon071.jpg';
+var imageHelper = require('../helpers/image');
+// var testImage = 'http://orig01.deviantart.net/92d3/f/2010/110/7/2/pla_sf_3_by_recon071.jpg';
 exports.image = (req, res) => {
-  // res.send('main');
-  imageHelper.resizeImage(testImage, [
-    [2000, 2000]
-  ])
-  .then((thumbnailPaths) => res.send('<img src="http://storage.googleapis.com/tudor-filipovici/output-2000x2000.jpg">'))
-  .catch((err)=>console.log(err));
+  res.send('main');
+  // imageHelper.resizeImage(testImage, [
+  //   [2000, 2000]
+  // ])
+  // .then((thumbnailPaths) => res.send('<img src="http://storage.googleapis.com/tudor-filipovici/output-2000x2000.jpg">'))
+  // .catch((err)=>console.log(err));
 
 }
 
@@ -22,5 +22,5 @@ exports.transformations = function(req, res){
   var url = path.splice(2).join('/');
   imageHelper.resizeImage(url, [imageHelper.processTransformations(transformations)])
   .then((thumbnailPaths) => res.send(`<img src="http://storage.googleapis.com/tudor-filipovici/${thumbnailPaths}">`))
-  .catch((err)=>console.log(err));
+  .catch((err)=> res.send('Error fetching file'));
 }
